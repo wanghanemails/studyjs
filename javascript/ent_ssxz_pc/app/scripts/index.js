bd668917ffe46f3b19cf9547410db1f3e9374a94
wanghan1 <wanghan1@ifeng.com>
2017-02-22 18:04:16 +0800

    1

--- javascript/ent_ssxz_pc/app/scripts/index.js
+++ javascript/ent_ssxz_pc/app/scripts/index.js
@@ -18,24 +18,36 @@
                 var current_width = 0;
                 var current_height = 0;
 
-                console.log( window.innerWidth)
-                console.log( window.innerHeight)
+                console.log( window.innerWidth);
+                console.log( window.innerHeight);
 
                 var real_height = window.innerHeight-150-45-92;
 
 
 
               function chooseWH(){
+                  var height = true;
                   if( window.innerWidth<1000||(real_height)<458){
                       //最小屏  最小宽高
                       current_width = 1000;
                       current_height = 458;
+                      console.log(1);
+                      (((window.innerWidth/1000)-(real_height/458))>0) ? height=true : height=false ;
+
+                      if(height){
+
+                      }else{
+
+                      }
+
+
 
 
                   }else{
+                      console.log(2);
+                      (((window.innerWidth/1000)-(real_height/458))>0) ? height=true : height=false ;
 
-                      console.log((real_height)/458);
-                      (((window.innerWidth/1000)-(real_height/458))>0) ? console.log("高小以他为比例"):console.log("宽小以他为比例");
+                      //(((window.innerWidth/1000)-(real_height/458))>0) ? console.log("高小以他为比例"):console.log("宽小以他为比例");
 
 
 
@@ -47,7 +59,10 @@
                 chooseWH();
 
 
-                $(window).resize(function() {
+                $(window).resizeEnd({
+                    delay: 1000
+                }, function(){
+                    //处理代码
                    // 第二部  重绘加上动画。
                    chooseWH();
                 });
